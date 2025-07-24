29d0984dd3ad4c7544d66c463774d08700b91d63
Adam Hennessy <ahenneuk@live.com>
2025-07-24 18:03:05 +0100

    first working version

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
+import * as chrono from 'chrono-node';
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
 	mySetting: string;
+	appendCompletionDate: boolean;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	mySetting: 'default',
+	appendCompletionDate: true
 }
 
 export default class MyPlugin extends Plugin {
@@ -16,66 +19,59 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Task completion command.
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'toggle-task-complete',
+			name: 'Toggle task completion',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+				const cursor = editor.getCursor();
+				const line = editor.getLine(cursor.line);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				const isTodo = /^\s*-\s\[\s\]/.test(line);
+				const isDone = /^\s*-\s\[x\]/i.test(line);
+
+				if (!isTodo && !isDone) {
+					new Notice("Not a task line.");
+					return;
 				}
-			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+				let newLine = line;
+				const today = window.moment().format("YYYY-MM-DD");
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+				if (isTodo) {
+					newLine = newLine.replace("[ ]", "[x]");
+					if (this.settings.appendCompletionDate) {
+						newLine = newLine.replace(/\s*\[completed:: \[\[[^\]]*\]\]\]/, "");
+						newLine += ` [completed:: [[${today}]]]`;
+					}
+				} else {
+					newLine = newLine.replace("[x]", "[ ]");
+					if (this.settings.appendCompletionDate) {
+						newLine = newLine.replace(/\s*\[completed:: \[\[[^\]]*\]\]\]/, "");
+					}					
+				}
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+				editor.setLine(cursor.line, newLine);
+					}
+				});
+
+				
+		// Add task menu.
+		this.addCommand({
+			id: 'new-task',
+			name: 'New task',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+
+				const cursor = editor.getCursor();
+				const prefillText = editor.getLine(cursor.line).trim();
+
+				new CreateTaskModal(this.app, prefillText).open();
+
+				}
+			});
+
+		// Add settings.
+		this.addSettingTab(new SettingsTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,23 +87,154 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+
+class CreateTaskModal extends Modal {
+	descriptionInput: HTMLInputElement;
+	selectedPriority: string;
+	tagInput: HTMLInputElement;
+	dueInput: HTMLInputElement;
+	tagsInput: HTMLInputElement;
+	scheduledInput: HTMLInputElement;
+	private prefillText: string;
+
+	constructor(app: App, prefillText: string = "") {
 		super(app);
+		this.prefillText = prefillText;
+		this.selectedPriority = 'normal';
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
+
+		// Task name.
+		const taskRow = contentEl.createDiv({ cls: 'ticked-off-row' });
+
+		const descLabel = taskRow.createEl('label', { text: 'Task label:' });
+		descLabel.addClass("ticked-off-label");
+		
+		this.descriptionInput = taskRow.createEl('input', { type: 'text', placeholder: 'Enter task name' });
+		this.descriptionInput.addClass('ticked-off-text');
+		this.descriptionInput.value = this.prefillText;
+
+
+		// Priority buttons row.
+		const priorityRow = contentEl.createDiv({ cls: 'ticked-off-row' });
+
+		const priorityLabel = priorityRow.createEl('label', { text: 'Priority:' });
+		priorityLabel.addClass("ticked-off-label");
+
+		const buttonContainer = priorityRow.createDiv({ cls: 'ticked-off-priority-buttons' });
+
+		const priorities = ['low', 'normal', 'high', 'urgent'];
+		priorities.forEach(priority => {
+			const button = buttonContainer.createEl('button', { text: priority });
+			button.addClass('ticked-off-button');
+			if (priority === this.selectedPriority) {
+				button.addClass('selected');
+			}
+
+			button.addEventListener('click', () => {
+				this.selectedPriority = priority;
+				buttonContainer.querySelectorAll('button').forEach(btn => btn.classList.remove('selected'));
+				button.addClass('selected');
+			});
+		});
+
+
+		// Due input
+		const dueRow = contentEl.createDiv({ cls: 'ticked-off-row' });
+
+		const dueLabel = dueRow.createEl('label', { text: 'Due date:' });
+		dueLabel.addClass("ticked-off-label");
+
+		this.dueInput = dueRow.createEl('input', { type: 'text', placeholder: "e.g. 'friday', 'tomorrow', '24/04'" });
+		// this.dueInput.addClass("ticked-off-text");	
+
+		this.dueInput.addEventListener('blur', () => {
+			const dueRaw = this.dueInput.value.trim();
+			const parsedDate = chrono.parseDate(dueRaw, new Date(), { forwardDate: true });
+			if (dueRaw == "") {
+				this.dueInput.removeClass('ticked-off-invalid-date');
+			}
+			else if (parsedDate) {
+				this.dueInput.removeClass('ticked-off-invalid-date');
+				this.dueInput.value = window.moment(parsedDate).format("YYYY-MM-DD");
+			} else {
+				this.dueInput.addClass('ticked-off-invalid-date');
+			}
+		});
+		
+		// Due input
+		const scheduleRow = contentEl.createDiv({ cls: 'ticked-off-row' });
+
+		const scheduleLabel = scheduleRow.createEl('label', { text: 'Scheduled:' });
+		scheduleLabel.addClass("ticked-off-label");
+
+		this.scheduledInput = scheduleRow.createEl('input', { type: 'text', placeholder: "e.g. 'friday', 'tomorrow', '24/04'" });
+
+		this.scheduledInput.addEventListener('blur', () => {
+			const scheduleRaw = this.scheduledInput.value.trim();
+			const parsedDate = chrono.parseDate(scheduleRaw, new Date(), { forwardDate: true });
+			if (scheduleRaw == "") {
+				this.scheduledInput.removeClass('ticked-off-invalid-date');
+			}
+			else if (parsedDate) {
+				this.scheduledInput.removeClass('ticked-off-invalid-date');
+				this.scheduledInput.value = window.moment(parsedDate).format("YYYY-MM-DD");
+			} else {
+				this.scheduledInput.addClass('ticked-off-invalid-date');
+			}
+		});
+
+
+		// Task name.
+		const tagsRow = contentEl.createDiv({ cls: 'ticked-off-row' });
+
+		const tagsLabel = tagsRow.createEl('label', { text: 'Tags:' });
+		tagsLabel.addClass("ticked-off-label");
+		
+		this.tagsInput = tagsRow.createEl('input', { type: 'text', placeholder: "space or comma separated tags, e.g.: 'work, home'" });
+		this.tagsInput.addClass('ticked-off-text');
+
+
+		// Submit button
+		const submitRow = contentEl.createDiv({ cls: 'ticked-off-row'})
+
+		const submitBtn = submitRow.createEl('button', { text: 'Create Task' });
+		submitBtn.addClass("ticked-off-submit");
+		submitBtn.addEventListener('click', (e) => {
+			e.preventDefault();
+			const desc = this.descriptionInput.value.trim();
+			const priority = this.selectedPriority;
+			const due = this.dueInput.value;
+			const schedule = this.scheduledInput.value;
+			const tags = this.tagsInput.value.replace(" ", "#").replace(",", "");
+
+			const editor = this.app.workspace.activeEditor?.editor;
+
+			if (editor) {
+				let taskLine = `- [ ] ${desc}`;
+				
+				if (priority) {taskLine += ` [priority:: ${priority}]`;
+				if (due) taskLine += ` [due:: [[${due}]]]`;
+				if (schedule) taskLine += ` [schedule:: [[${schedule}]]]`;
+				if (tags) taskLine += ` [tags::#${tags}]`
+
+				editor.replaceRange(taskLine, editor.getCursor());
+
+			}
+			// console.log('CreateTaskModal submit:', { description: desc, priority, due, schedule });
+			this.close();
+		}});
+
+
+
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
+
+class SettingsTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -121,13 +248,12 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Task complete creation date')
+			.setDesc('Toggle whether the task completion command will append/remove a \'compeleted\' field.')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.appendCompletionDate)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.appendCompletionDate = value;
 					await this.plugin.saveSettings();
 				}));
 	}
